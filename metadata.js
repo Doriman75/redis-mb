62f0d42184eeaac1e040b567c9ed13890d88a018
Doriman75 <pierantonio.manco@gmail.com>
2018-07-23 17:37:45 +0200

    added support for single header

--- metadata.js
+++ metadata.js
@@ -1,6 +1,6 @@
 const uuidv4 = require('uuid/v4');
 module.exports = {
-  "functions": {
+  "fixed": {
     "id": function(body, req, res) {
       body.id = uuidv4();
       return body;
@@ -14,12 +14,31 @@ module.exports = {
       return body;
     }
   },
+  "params": {
+    "header": function(body, req, res, param) {
+      body[param] = req.get(param);
+      return body;
+    }
+  },
   "add": function(message, metadata, req, res) {
-    Object.keys(this.functions)
-      .filter(e => (metadata == "all" || metadata.indexOf(e) != -1))
-      .reduce((message, k) => {
-        return this.functions[k](message, req, res)
-      }, message);
+    var metadatas = metadata != "all" ? metadata.split(",").map(e => e.trim()) : Object.keys(this.fixed);
+
+    metadatas
+      .filter(e => e.indexOf("[") == -1)
+      .filter(k => this.fixed[k])
+      .forEach(k => message = this.fixed[k](message, req, res));
+
+    metadatas
+      .filter(e => e.indexOf("[") != -1)
+      .map(k => {
+        return {
+          method: k.substring(0, k.indexOf('[')),
+          param: k.substring(1 + k.indexOf('['), k.indexOf(']'))
+        }
+      })
+      .filter(k => this.params[k.method])
+      .forEach(k => message = this.params[k.method](message, req, res, k.param));
+
     return message;
   }
 };
